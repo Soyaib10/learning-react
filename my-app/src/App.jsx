5cc8ea5d9c8d482f96227d70d76e431ce0c051c5
Soyaib10 <soyaibzihad10@gmail.com>
2025-02-09 12:08:52 +0600

    made a dynamically changing message based on time

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,36 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react-logo.png';
 import './App.css'
 
-const fName = "soyaib"
-const lName = "zihad"
-
 function App() {
-  const currentYear = new Date().getFullYear()
+  const currentDate = new Date(2019, 1, 1, 19)
+  const currentHour = currentDate.getHours();
+
+  const customStyle = {
+    color : "black"
+  }
+
+  let message = ""
+  if (currentHour > 0 && currentHour <= 12) {
+    message = "Good Morning"
+    customStyle.color = "black"
+  } else if (currentHour > 12 && currentHour <= 18) {
+    message = "Good Afternoon"
+    customStyle.color = "yellow"
+  } else {
+    message = "Good Evening"
+    customStyle.color = "red"
+  }
 
   return (
     <div>
-      <p>Created {fName + " " + lName}</p>
-      
-      <img 
-        src="https://images.pexels.com/photos/63764/pexels-photo-63764.jpeg" 
-        alt="Lamborghini Aventador" 
-        className="image-style" 
-      />
-      
-      <img 
-        src="https://images.pexels.com/photos/192130/pexels-photo-192130.jpeg" 
-        alt="Car Wallpapers" 
-        className="image-style" 
-      />
-      
-      <img 
-        src="https://forumstatic.oneplusmobile.com/opforum-gl/upload/image/app/thread/20231112/4318020878898572541/1462181598320394244/1462181598320394244.jpeg?x-ocs-process=image/format,webp/resize,w_720" alt="Car Wallpapers" 
-        className="image-style" 
-      />
-      
-      <p>Copyright ©{currentYear}</p>
+      <h1 className='greeting' style={customStyle}>Assalamualaikum, {message}</h1>
     </div>
   );
-}
+} 
 
 export default App;
