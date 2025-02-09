5bc66d24e67d05281d2d5e66e52f566a6edbd391
Soyaib10 <soyaibzihad10@gmail.com>
2025-02-09 11:23:49 +0600

    learned src and other inline

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,15 +6,32 @@ const fName = "soyaib"
 const lName = "zihad"
 
 function App() {
+  const currentYear = new Date().getFullYear()
+
   return (
     <div>
-      <h1>Amar valo lage na</h1>
-      <p>My lucky number is {fName + " " + lName}  </p>
+      <p>Created {fName + " " + lName}</p>
+      
+      <img 
+        src="https://images.pexels.com/photos/63764/pexels-photo-63764.jpeg" 
+        alt="Lamborghini Aventador" 
+        className="image-style" 
+      />
+      
+      <img 
+        src="https://images.pexels.com/photos/192130/pexels-photo-192130.jpeg" 
+        alt="Car Wallpapers" 
+        className="image-style" 
+      />
+      
+      <img 
+        src="https://forumstatic.oneplusmobile.com/opforum-gl/upload/image/app/thread/20231112/4318020878898572541/1462181598320394244/1462181598320394244.jpeg?x-ocs-process=image/format,webp/resize,w_720" alt="Car Wallpapers" 
+        className="image-style" 
+      />
+      
+      <p>Copyright ©{currentYear}</p>
     </div>
   );
 }
 
 export default App;
-
-
-
