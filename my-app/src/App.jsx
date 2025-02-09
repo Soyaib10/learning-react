f3a751f3408b1a4c03182ecd3bf90352eefea9b4
Soyaib10 <soyaibzihad10@gmail.com>
2025-02-09 10:49:10 +0600

    added my first react project

---
+++ my-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react-logo.png';
+import './App.css'
+
+const fName = "soyaib"
+const lName = "zihad"
+
+function App() {
+  return (
+    <div>
+      <h1>Amar valo lage na</h1>
+      <p>My lucky number is {fName + " " + lName}  </p>
+    </div>
+  );
+}
+
+export default App;
+
+
+
