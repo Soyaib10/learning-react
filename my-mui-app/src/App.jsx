2f1ae2621021172c5b8290253bfbee7cffc91d7d
Soyaib10 <soyaibzihad10@gmail.com>
2025-03-03 16:30:34 +0600

    implemented other details

--- my-mui-app/src/App.jsx
+++ my-mui-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import InputComponents from "./components/InputComponents";
 import DataDisplayComponents from "./components/DisplayDataComponents";
+import TableComponents from "./components/TableComponents";
 
 function App() {
   return (
     <div>
       {/* <InputComponents /> */}
       <DataDisplayComponents />
+      <TableComponents />
     </div>
   )
 }
